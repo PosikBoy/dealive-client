c8df1836f6d1830e8034f9599e047a076cffc409
PosikBoy <z.kalinichenko4@gmail.com>
2024-12-21 07:16:05 +0300

    Добавил яндекс метрику Добавил пробел на странице о нас

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,8 @@ import { StoreProvider } from "@/components/StoreProvider";
 import Footer from "@/components/Layout/Footer/Footer";
 import { Metadata, Viewport } from "next";
 import CookieAgreement from "@/components/Ui/CookieAgreement/CookieAgreement";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -59,6 +61,28 @@ export default function RootLayout({
             <Footer />
           </div>
           <CookieAgreement />
+          <Script>
+            {`
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(99278339, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`}
+          </Script>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/99278339"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
         </body>
       </StoreProvider>
     </html>

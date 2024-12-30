e271765aa0eacef928d02f57db284edb61c19b17
PosikBoy <z.kalinichenko4@gmail.com>
2024-12-30 18:35:03 +0300

    add generating sitemap.xml

---
+++ next-sitemap.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: "https://dealive.ru", // Укажите ваш сайт
+  generateRobotsTxt: true, // Опционально, если хотите автоматически создать robots.txt
+  // Другие опции конфигурации можно настроить, если необходимо
+};

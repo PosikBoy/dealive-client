3d938e65c4bbf1da5251adcb5255e1dc9284e4ab
PosikBoy <z.kalinichenko4@gmail.com>
2025-01-06 18:33:52 +0300

    Поменял конфигурацию sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   siteUrl: "https://dealive.ru",
   exclude: ["/login", "/profile"],
+  transform: async (config, path) => {
+    if (path === "/") {
+      return {
+        loc: path,
+        priority: 1.0, // Главная страница с самым высоким приоритетом
+        changefreq: "daily",
+      };
+    }
+
+    if (path === "/order") {
+      return {
+        loc: path,
+        priority: 0.9, // Низкий приоритет для страницы с политикой
+        changefreq: "weekly",
+      };
+    }
+
+    return {
+      loc: path,
+      priority: 0.7, // Стандартный приоритет для остальных страниц
+      changefreq: "weekly",
+    };
+  },
 };

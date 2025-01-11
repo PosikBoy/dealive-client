31a8c0d647d19978065d3910775b89a397e395c8
PosikBoy <z.kalinichenko4@gmail.com>
2025-01-11 06:16:44 +0300

    Добавил страницу сбор товаров с пвз Переделал блок что мы доставляем на флекс-бокс, поменял анимацию Вынес все сео-компоненты в отдельную папку. Теперь они не хранятся внутри каждой страницы.

---
+++ src/components/screens/PickupPVZPage/PickupPVZ.tsx
@@ -0,0 +1,69 @@
+import "./PickupPVZ.scss";
+import Tile from "@/components/Ui/Tile/Tile";
+import { FC } from "react";
+import Heading from "@/components/Ui/Heading/Heading";
+import Heading2 from "@/components/Ui/Heading2/Heading2";
+import Heading3 from "@/components/Ui/Heading3/Heading3";
+
+import HowToUse from "@/components/seo/HowToUse/HowToUse";
+import AdditionalService from "@/components/seo/AdditionalService/AdditionalService";
+import WhatWeDeliver from "@/components/seo/WhatWeDeliver/WhatWeDeliver";
+import MiniForm from "@/components/Ui/MiniForm/MiniForm";
+
+const PickupPVZPage: FC = () => {
+  return (
+    <main className="page-content">
+      <section className="welcome">
+        <div className="container">
+          <div className="welcome__content">
+            <div className="welcome__block">
+              <div className="welcome__offer">
+                <strong>
+                  <Heading className="welcome__title">
+                    Сбор товаров и выкупов с пунктов выдачи заказов (ПВЗ) в
+                    Москве
+                  </Heading>
+                </strong>
+                <strong>
+                  <Heading2 className="welcome__subtitle">
+                    Вы сами назначаете цену
+                  </Heading2>
+                </strong>
+              </div>
+              <div className="welcome__mini-form">
+                <MiniForm />
+              </div>
+            </div>
+            <strong>
+              <Heading3 className="welcome__losung">
+                Почему выбирают нас для сбора товаров из ПВЗ?
+              </Heading3>
+            </strong>
+
+            <div className="welcome__tiles">
+              <Tile title="Оперативность и точность" />
+              <Tile title="Экономия времени" />
+              <Tile title="Гибкость цены" />
+              <Tile title="Простота заказа" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <WhatWeDeliver
+        title="Что мы собираем?"
+        description="Мы готовы забирать товары с ПВЗ, такие как одежда, электроника, косметика, книги и многое другое. Предоставляем услуги по сбору предварительно оплаченных заказов. Курьеры не смогут оплатить заказы со своего счета."
+      />
+      <HowToUse
+        title="Как оформить заказ на сбор с ПВЗ?"
+        description="Мы осуществляем услуги сбора товаров и выкупов с пунктов выдачи заказов (ПВЗ). Наши курьеры заберут все товары быстро и без задержек, а вы можете сосредоточиться на других задачах или личных делах. Работаем только с уже оплаченными заказами. Курьеры не смогут оплатить товар со своего счета."
+        step1={`В поле \"Что везем?\" укажите товары для сбора. Если товары разные - так и напишите. В дополнительную информацию вы можете указать коды получения или ссылки на QR-коды. Они станут доступны курьеру только после взятия заказа. Заполните остальные данные и нажмите \"Оформить заказ\". `}
+        step2="Мы начинаем поиск курьера на сбор. Обычно это занимает не более 20 минут. Если хотите ускорить процесс, предложите чуть большую сумму за заказ. Ориенторовочный тариф на сбор - 200р за точку, при итоговом весе не более 0.5 кг за точку "
+        step3="Ваш заказ в процессе! Курьер собирает товары с  ПВЗ и готовит их для доставки или передачи. Вы можете отслеживать его маршрут в режиме реального времени. Далее курьер отвезет все товары на ваш склад."
+        step4="Сбор завершён! Курьер вовремя забрал и доставил товары. Мы уверены, что всё прошло оперативно и безопасно."
+      />
+      <AdditionalService />
+    </main>
+  );
+};
+
+export default PickupPVZPage;

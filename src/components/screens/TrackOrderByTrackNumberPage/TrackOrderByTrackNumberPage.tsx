319222075639e53b4203568a1909effa9a1ad7a0
PosikBoy <z.kalinichenko4@gmail.com>
2025-01-22 22:35:09 +0300

    Начал работу по рефактору кода

--- src/components/screens/TrackOrderByTrackNumberPage/TrackOrderByTrackNumberPage.tsx
+++ src/components/screens/TrackOrderByTrackNumberPage/TrackOrderByTrackNumberPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { FC, useEffect, useState } from "react";
-import "./TrackOrderByTrackNumberPage.scss";
+import styles from "./TrackOrderByTrackNumberPage.module.scss";
 
 import orderService from "@/services/order/order.service";
 import { IOrder } from "@/types/order.interface";
